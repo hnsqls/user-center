a8e297fd5c02d4eecd7f216ded0762a1261cce16
hnsqls <2661108038@qq.com>
2025-03-02 17:09:39 +0800

    add pina 全局状态管理

--- user-center-vue/src/App.vue
+++ user-center-vue/src/App.vue
@@ -7,5 +7,10 @@
 <style></style>
 
 <script setup lang="ts">
-import BasicLayouts from "./layouts/BasicLayouts.vue";
+import BasicLayouts from "@/layouts/BasicLayouts.vue";
+import { useLoginUserStore } from "@/store/useLoginUserStore";
+
+const loginUserStore = useLoginUserStore();
+loginUserStore.fetchLoginUser();
+
 </script>
