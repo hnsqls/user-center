4e62cc3f328fb06ac14abf9986a105a94144730c
hnsqls <2661108038@qq.com>
2025-03-02 16:39:57 +0800

    add axios 请求接口user.ts

--- user-center-vue/src/api/user.ts
+++ user-center-vue/src/api/user.ts
@@ -1,10 +1,11 @@
 // import myAxios from "@/request";
 
-import myAxios from "@/request"
+import myAxios from "@/request";
 
 // /**
-//  * 用户注册
+//  * 用户注册接口
 //  * @param params
+//  * @returns
 //  */
 // export const userRegister = async (params: any) => {
 //   return myAxios.request({
@@ -14,75 +15,149 @@ import myAxios from "@/request"
 //   });
 // };
 
-export const userRegister = async(params :any) =>{
-    return myAxios.request({
-        url:"/api/user/register",
-        method:"POST",
-        data:params,
-    })
-
-}
-
 // /**
 //  * 用户登录
 //  * @param params
+//  * @returns
 //  */
 // export const userLogin = async (params: any) => {
-//   return myAxios.request({
+//   const response = await myAxios.request({
 //     url: "/api/user/login",
 //     method: "POST",
 //     data: params,
 //   });
+
+//   return response;
 // };
 
 // /**
-//  * 用户注销
+//  * 注销登出
 //  * @param params
+//  * @returns
 //  */
 // export const userLogout = async (params: any) => {
-//   return myAxios.request({
+//   const response = await myAxios.request({
 //     url: "/api/user/logout",
 //     method: "POST",
 //     data: params,
 //   });
+
+//   return response;
 // };
 
 // /**
-//  * 获取当前用户
+//  * 获取当前登录用户信息
+//  * @param userid
 //  */
 // export const getCurrentUser = async () => {
-//   return myAxios.request({
+//   const response = await myAxios.request({
 //     url: "/api/user/current",
 //     method: "GET",
 //   });
+
+//   return response;
 // };
 
 // /**
-//  * 获取用户列表
+//  * 根据username 获取用户列表
 //  * @param username
 //  */
-// export const searchUsers = async (username: any) => {
-//   return myAxios.request({
+// export const searchUsers = async (username: string) => {
+//   const response = await myAxios.request({
 //     url: "/api/user/search",
 //     method: "GET",
-//     params: {
-//       username,
-//     },
+//     data: username,
 //   });
+
+//   return response;
 // };
 
 // /**
 //  * 删除用户
-//  * @param id
+//  * @param userId
 //  */
-// export const deleteUser = async (id: string) => {
-//   return myAxios.request({
-//     url: "/api/user/delete",
+// export const deleteUser = async (userId: string) => {
+//   const response = await myAxios.request({
+//     url: `/api/user/delete/${userId}`,
 //     method: "POST",
-//     data: id,
-//     // 关键点：要传递 JSON 格式的值
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
 //   });
+
+//   return response;
 // };
+
+// 以下是参考
+
+/**
+ * 用户注册
+ * @param params
+ */
+export const userRegister = async (params: any) => {
+  return myAxios.request({
+    url: "/api/user/register",
+    method: "POST",
+    data: params,
+  });
+};
+/**
+ * 用户登录
+ * @param params
+ */
+export const userLogin = async (params: any) => {
+  return myAxios.request({
+    url: "/api/user/login",
+    method: "POST",
+    data: params,
+  });
+};
+
+/**
+ * 用户注销
+ * @param params
+ */
+export const userLogout = async (params: any) => {
+  return myAxios.request({
+    url: "/api/user/logout",
+    method: "POST",
+    data: params,
+  });
+};
+
+/**
+ * 获取当前用户
+ */
+export const getCurrentUser = async () => {
+  return myAxios.request({
+    url: "/api/user/current",
+    method: "GET",
+  });
+};
+
+/**
+ * 获取用户列表
+ * @param username
+ */
+export const searchUsers = async (username: any) => {
+  return myAxios.request({
+    url: "/api/user/search",
+    method: "GET",
+    params: {
+      username,
+    },
+  });
+};
+
+/**
+ * 删除用户
+ * @param id
+ */
+export const deleteUser = async (id: string) => {
+  return myAxios.request({
+    url: "/api/user/delete",
+    method: "POST",
+    data: id,
+    // 关键点：要传递 JSON 格式的值
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
